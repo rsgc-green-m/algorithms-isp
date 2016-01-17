b770b816b55e134941ba2f91cd5d74dd7b7100d8
Mike <mgreen@rsgc.on.ca>
2016-01-17 11:31:14 -0500

    Sucsess!
    I was able to get my code to work and convert a number. i am now going
    to try and add a little bit of an interface.

--- Scientific Notation .playground/Contents.swift
+++ Scientific Notation .playground/Contents.swift
@@ -1,1 +1,28 @@
+import Darwin
 
+// a number to test
+var number = "47000000"
+
+// get the string length
+var count = number.characters.count
+print("Count of characters is \(count)")
+
+// loop over each character in a string
+for c in number.characters {
+    print("\(c)")
+}
+
+// get the exponent
+var exponent = count - 1
+
+// get the divisor
+var divisor = pow(Float(10), Float(exponent))
+
+// get the dividend
+var dividend = Float(number)!
+
+// get the coefficient for scientific notation
+var coefficient = dividend / divisor
+
+// build the scientific notation 
+var scientificNotation = String(coefficient) + " x 10^" + String(exponent)
